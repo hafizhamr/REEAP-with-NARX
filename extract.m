78f3d0c48ab778e6b52bc6dbf26298c3911e2e34
hafizhamr <hafizhamr.hemhr@gmail.com>
2022-08-22 16:46:28 +0700

    Update extract.m

--- extract.m
+++ extract.m
@@ -1,30 +1,35 @@
-function [feature] = extract(emg, sudut, tresholdZC)
-    Ts = .001;
-    window = 100;
-    winSum = floor(length(emg) / window) - 1;  % Jumlah window
+function [feature] = extract(emg, sudut, tresholdZC, windowSize)
+    Fs = 10000;
+    Ts = 1/Fs;
+    window = windowSize/1000;               % s
+    dataInWindow = Fs*window;
+    winSum = floor(length(emg) / dataInWindow) - 1;  % Jumlah window
+    
+    emgs = zeros(1,dataInWindow);
+    suduts = zeros(1,dataInWindow);
+    zc = zeros(1,length(emg)/dataInWindow);
+    iemg = zeros(1,length(emg)/dataInWindow);
+    sudutS = zeros(1,length(emg)/dataInWindow);
     
     % Looping setiap window
     for k = 0:winSum                           
         % Inisialisasi data pada masing-masing window dari data awal
-        j = k * window;
-        for jj = 1:window               % Looping untuk windowing
+        j = k * dataInWindow;
+        for jj = 1:dataInWindow         % Looping untuk windowing
             emgs(jj) = emg(j+jj);       % Ambil data EMG untuk 1 window
             suduts(jj) = sudut(j+jj);   % Ambil data Sudut untuk 1 window
         end
         % Hitung banyaknya Zero Crossing dalam 1 window
-        zc(k+1) = jZC(emgs, tresholdZC);
+        zc(k+1) = jZC(emgs*dataInWindow, tresholdZC);
         % Hitung nilai Integrated EMG dalam satu window
         iemg(k+1) = sum(abs(emgs));  
         % Ambil data sudut dalam 1 window
-        sudutS(k+1) = suduts(window);       
+        sudutS(k+1) = suduts(dataInWindow);
     end
     
-    ts = 0:(Ts*window):Ts*window*winSum; %Time sampling
+    ts = 0:(Ts*dataInWindow):Ts*dataInWindow*winSum; %Time sampling
     feature.zc = zc';
     feature.iemg = iemg';
     feature.sudut = sudutS';
     feature.ts = ts';
-    clear 'zc';
-    clear 'iemg';
-    clear 'sudutS';
 end
