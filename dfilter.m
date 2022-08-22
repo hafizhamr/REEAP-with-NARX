2afbcd6f21b6fb56ad0d7b320407fe36d1ea02d8
hafizhamr <hafizhamr.hemhr@gmail.com>
2022-08-22 16:45:53 +0700

    Update dfilter.m

--- dfilter.m
+++ dfilter.m
@@ -1,27 +1,49 @@
-%% Kode Fungsi dfilter
-function filtered_emg = dfilter(emg)
+function filtered_emg = dfilter(emg, varargin)
     % Filter sinyal EMG dengan Butterworth IIR Filter
     % Bandpass filter orde 2 dengan rentang frekuensi 10Hz - 500 Hz
     % Notch Filter di frekuensi 50Hz untuk PLI
     
-    fs = 1000;                         % Sampling rate
-    wnb = [20 400] / (fs / 2);          % Frekuensi cut-off bandpass
-    wnn = [49.98 50.02] / (fs / 2);     % Frekuensi cut-off notch
+    fs = 10000;                         % Sampling rate
+    wnb = [10 450] / (fs / 2);          % Frekuensi cut-off bandpass
+    wnn = [49 51] / (fs / 2);           % Frekuensi cut-off notch
     
     % Bandpass filter
-    [b, a] = butter(1, wnb, 'bandpass');
-    emg_f = filtfilt(b, a, emg);
-    %figure(3)
-    %grpdelay(b, a, [])
+    [z,p,k] = butter(4, wnb, 'bandpass');
+    [sos,g] = zp2sos(z,p,k);
+    % fvtool(sos, 'Analysis', 'freq', 'Fs', 10000)
+    filtered_emg = filtfilt(sos, g, emg);
 
     % Notch filter
-    [b, a] = butter(1, wnn, 'stop');
-    filtered_emg = filtfilt(b, a, emg_f);
-    
-%     % Rectification of filtered EMG signal
-%     filtered_emg = abs(filtered_emg);
-%     
-%     % Envelope / IEMG
-%     [filtered_emg,~] = envelope(filtered_emg, 100, 'peak');
-%     filtered_emg = abs(filtered_emg);
+    [z,p,k] = butter(2, wnn, 'stop');
+    [sos,g] = zp2sos(z,p,k);
+    % fvtool(sos, 'Analysis', 'freq', 'Fs', 10000)
+    filtered_emg = filtfilt(sos, g, filtered_emg);
+
+    if nargin == 1
+        useFFT = 'false';
+    else
+        useFFT = "true";
+    end
+
+    if useFFT == "true"
+        L = length(emg);
+        f = fs*(0:(L/2))/L;
+        
+        Y = fft(emg);
+        P2 = abs(Y/L);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        
+        Y2 = fft(filtered_emg);
+        P4 = abs(Y2/L);
+        P3 = P4(1:L/2+1);
+        P3(2:end-1) = 2*P3(2:end-1);
+        
+        plotFigure(f, [], P1, P3, 'Raw EMG dan Filtered EMG', ...
+            'Raw EMG', 'Filtered EMG', 'true')
+        ylim([0 0.0015])
+        xlim([0 700])
+        grid on
+    else
+    end
 end
